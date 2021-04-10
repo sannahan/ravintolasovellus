a369ffd7f073a847337daa66733ac7dc43579acc
sannahan <73829699+sannahan@users.noreply.github.com>
2021-04-10 14:53:55 +0300

    Korjaa schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
 CREATE TABLE restaurants (id SERIAL PRIMARY KEY, name TEXT, description TEXT, address TEXT, opening TEXT, closing TEXT);
-CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, stars INTEGER, choice TEXT);
+CREATE TABLE reviews (id SERIAL PRIMARY KEY, restaurant_id INTEGER REFERENCES restaurants, stars INTEGER, comment TEXT);
