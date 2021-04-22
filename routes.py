167dd0f09593d52a5189fff153f772b1d35a08d7
sannahan <73829699+sannahan@users.noreply.github.com>
2021-04-22 16:50:05 +0300

    Korjaa parametrin välittäminen

--- routes.py
+++ routes.py
@@ -40,11 +40,11 @@ def signup():
 def map():
 	info = restaurants.get_info_for_map()
 	test = [
-      		['Bondi Beach', -33.890542, 151.274856, 4],
-      		['Coogee Beach', -33.923036, 151.259052, 5],
-      		['Cronulla Beach', -34.028249, 151.157507, 3],
-      		['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      		['Maroubra Beach', -33.950198, 151.259302, 1]
+      		['Bondi Beach', -33.890542, 151.274856],
+      		['Coogee Beach', -33.923036, 151.259052],
+      		['Cronulla Beach', -34.028249, 151.157507],
+      		['Manly Beach', -33.80010128657071, 151.28747820854187],
+      		['Maroubra Beach', -33.950198, 151.259302]
     	]
 	return render_template("map.html", test=test)
 
